ad3cf3977a3b3f03278d137c1c2ff8650c2454a0
MehdiGrimault <mehdi.grimault@gmail.com>
2023-10-23 16:40:52 +0200

    i add react to do the front end of the app, and i displayed the envelopes

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import { Button, Stack } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Envelope from "./components/Envelope.js";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [envelopes, setEnvelopes] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:5500/envelopes")
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        setEnvelopes(result.envelopes)
+      });
+  })
+
+  return (<Container className="my-4">
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">My budget</h1>
+      <Button variant="primary">Create a new envelope</Button>
+      <Button variant="outline-primary">Transfer budget</Button>
+    </Stack>
+    <div style={{
+      display: "grid", gridTemplateColumns: "repeat(auto-fill,minmax(300px, 1fr))",
+      gap: "1rem",
+      alignItems: "flex-start"
+    }}>
+       {envelopes.map((envelope) => (
+          <Envelope title={envelope.title} expense={0} budget={envelope.budget} gray />
+        ))}
+    </div>
+  </Container>
+  );
+}
+
+export default App;
